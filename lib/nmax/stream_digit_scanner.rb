853bbc13895d769b5b12350fe806d8189fe122b3
Ilya Morozov <imorozov@n-core.ru>
2017-01-10 14:24:52 +0300

    Specs and fixed for StreamDigitScanner

--- lib/nmax/stream_digit_scanner.rb
+++ lib/nmax/stream_digit_scanner.rb
@@ -1,7 +1,8 @@
 module Nmax
   class StreamDigitScanner
-    NUMBERS = '0'..'9'
-    REGEX   = /\d+/
+    NUMBERS    = '0'..'9'
+    REGEX      = /\d+/
+    READ_LIMIT = 2000 # bytes
 
     def initialize(stream)
       @stream         = stream
@@ -12,26 +13,22 @@ module Nmax
       while chunk = next_chunk
         scan_chunk(chunk).each(&block)
       end
+      scan_chunk('').each(&block)
     end
 
     def scan_chunk(chunk)
-      matches = chunk.scan(REGEX)
-      if NUMBERS.cover? @chunk[-1]
-        set_chunk_part(matches.pop)
+      matches = (@previous_chunk + chunk).scan(REGEX)
+      if NUMBERS.cover? chunk[-1]
+        @previous_chunk = matches.pop
+      else
+        @previous_chunk = ''
       end
       matches
     end
 
     def next_chunk
-      @chunk = @previous_chunk + @stream.read(10).to_s
-      puts "buffer is now: #{@chunk.inspect}"
-      @previous_chunk = ''
-      return nil if @chunk.empty?
-      @chunk
-    end
-
-    def set_chunk_part(part)
-      @previous_chunk = part
+      return nil if @stream.eof?
+      @stream.read(READ_LIMIT)
     end
   end
 end
