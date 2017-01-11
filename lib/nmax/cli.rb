3f40c7c7e6d61c5d94a6d1246db11de2a31a1ed8
Ilya Morozov <imorozov@n-core.ru>
2017-01-11 14:52:01 +0300

    Added error/help message to CLI

--- lib/nmax/cli.rb
+++ lib/nmax/cli.rb
@@ -1,14 +1,35 @@
 module Nmax
   class CLI
+    INFO_MESSAGE = %(
+nmax outputs N largest numbers found in STDIN.
+nmax accepts single argument N -- maximum count of numbers to show.
+    )
     def self.start(argv)
-      nmax = argv.fetch(0).to_i
+      new(argv).run
+    end
 
-      top = TopNArray.new(nmax)
+    def initialize(arguments)
+      @arguments = arguments
+      @nmax = nmax
+    end
+
+    def run
+      top = TopNArray.new(@nmax)
       StreamDigitScanner.new(STDIN).scan do |i|
         top << i.to_i
       end
 
+      puts "Top #{@nmax} largest numbers:"
       puts top.to_a
+    end
+
+    def nmax
+      n = @arguments[0].to_i
+      unless n > 0
+        STDERR.puts INFO_MESSAGE
+        exit(1)
+      end
+      n
     end
   end
 end
